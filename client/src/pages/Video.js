6a1bf6627481c481099ff0a39ccd75e741cdd48a
DESKTOP-3R5KC0E\admin <ngk.khang94@gmail.com>
2023-02-09 14:12:13 +0700

    fix react-dom

--- client/src/pages/Video.js
+++ client/src/pages/Video.js
@@ -39,9 +39,9 @@ export default function Video() {
             height="315"
             src={`https://www.youtube.com/embed/${videoById && videoById.link}`}
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </div>
         <div className="comments">
